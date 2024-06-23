bb341fce40ba882c177d089f3f43182591bdbda9
v2balasu <vithuranb@gmail.com>
2024-06-22 22:26:47 -0400

    perist client

--- app/server.rb
+++ app/server.rb
@@ -10,9 +10,11 @@ class YourRedisServer # rubocop:disable Style/Documentation
   end
 
   def start
+    client = server.accept
     loop do
-      client = server.accept
       client.puts "+PONG\r\n"
+    rescue StandardError
+      client = server.accept
     end
   end
 end
