126d13adedbe380255489ecd00fda42ab92d3861
v2balasu <vithuranb@gmail.com>
2024-06-29 12:11:06 -0400

    Added replicaof option

--- app/server.rb
+++ app/server.rb
@@ -1,13 +1,14 @@
+require 'optparse'
 require 'socket'
 require_relative './client_connection'
 require_relative './data_store'
 
 class YourRedisServer # rubocop:disable Style/Documentation
-  def initialize(port)
+  def initialize(port, master_address)
     @port = port
     @data_store = DataStore.new
     @server_info = {
-      role: 'master'
+      role: master_address.nil? ? 'master' : 'slave'
       # connected_slaves: 0,
       # master_replid: 0,
       # master_repl_offset: 0
@@ -39,7 +40,20 @@ class YourRedisServer # rubocop:disable Style/Documentation
   end
 end
 
-port = 6379
-port = ARGV[1].to_i if ARGV.first == '--port' && !ARGV[1].nil?
+options = {}
+OptionParser.new do |parser|
+  parser.banner = 'Usage: server.rb [options]'
+  parser.on('--port PORT', Integer, 'Specify the port number') { |p| options[:port] = p }
+  parser.on('--replicaof <MASTER_HOST>_<MASTER_PORT>', String, 'Specify the port number') do |address|
+    options[:master_address] = address
+  end
+end.parse!
 
-YourRedisServer.new(port).start
+master_address = options[:master_address]
+if !master_address.nil? && !master_address.match(/[A-z0-9]+\s[0-9]+/)
+  pp 'Invalid replication address'
+  exit
+end
+
+port = options[:port] || 6379
+YourRedisServer.new(port, master_address).start
